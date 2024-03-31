2d8685057bc7a8736fce263c9caedee9c10dfe67
Mehmet Efe Akça <efeakca@gmail.com>
2024-03-31 19:18:43 +0300

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+    if let Err(err) = listen_to_incoming(listener) {
+        println!("Error while listening: {:?}", err);
     }
+}
+
+fn listen_to_incoming(listener: TcpListener) -> Result<(), std::io::Error> {
+    for stream in listener.incoming() {
+        handle_connection(stream?);
+    }
+
+    Ok(())
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap(); // read 1K bytes for now
+
+    stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
